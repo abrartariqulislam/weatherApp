48ba20fb7d0a4fcf003385736306f72f5ee5a0a5
abrartariqulislam <islamtariqul010@gmail.com>
2023-11-12 12:38:08 +0600

    update

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-const city = document.querySelector(".city")
-const temp = document.querySelector(".temp")
-const humidity = document.querySelector(".humidity")
-const wind = document.querySelector(".wind")
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const searchBtn = document.querySelector(".searchBtn");
+const searchInput = document.querySelector(".search input");
+const display = document.querySelector(".display");
+const incorrectCity = document.querySelector(".incorrectCity p");
 
-const apiKey = "c92b59c23cc99d80c604119d57127a98"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=kushtia"
+const apiKey = "c92b59c23cc99d80c604119d57127a98";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
+display.style.display = "none";
+incorrectCity.style.display = "none";
 
-const checkWether = async () =>{
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    const data = await response.json()
-    city.textContent = data.name
-    temp.textContent = Math.round(data.main.temp) + "°c"
-    humidity.textContent = Math.round(data.main.humidity) + "%"
-    wind.textContent = Math.round(data.wind.speed) + " -km/h"
-}
+const checkWether = async (cityName) => {
+  try {
+    const response = await fetch(
+      apiUrl + `&q=${cityName}` + `&appid=${apiKey}`
+    );
+    const data = await response.json();
 
+    if (data.name) {
+      city.textContent = data.name;
+      temp.textContent = Math.round(data.main.temp) + "°c";
+      humidity.textContent = Math.round(data.main.humidity) + "%";
+      wind.textContent = Math.round(data.wind.speed) + " -km/h";
+      display.style.display = "block";
+      incorrectCity.style.display = "none";
+    } else {
+      display.style.display = "none";
+      incorrectCity.style.display = "block";
+    }
+  } catch (error) {
+    incorrectCity.style.display = "block";
+    incorrectCity.textContent = "Internal server error!";
+    incorrectCity.style.fontSize = "30px";
+  }
+};
 
-checkWether()
+searchBtn.addEventListener("click", () => {
+  checkWether(searchInput.value);
+});
