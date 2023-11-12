5b3432cc4a500f9e9e76398338159c48e8223d74
abrartariqulislam <islamtariqul010@gmail.com>
2023-11-12 16:37:19 +0600

    finished weather app

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const wind = document.querySelector(".wind");
 const searchBtn = document.querySelector(".searchBtn");
 const searchInput = document.querySelector(".search input");
 const display = document.querySelector(".display");
+const weatherIcon = document.querySelector(".weather_icon");
 const incorrectCity = document.querySelector(".incorrectCity p");
 
 const apiKey = "c92b59c23cc99d80c604119d57127a98";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-display.style.display = "none";
-incorrectCity.style.display = "none";
 
 const checkWether = async (cityName) => {
   try {
@@ -24,9 +23,31 @@ const checkWether = async (cityName) => {
       city.textContent = data.name;
       temp.textContent = Math.round(data.main.temp) + "°c";
       humidity.textContent = Math.round(data.main.humidity) + "%";
-      wind.textContent = Math.round(data.wind.speed) + " -km/h";
+      wind.textContent = data.wind.speed + " -km/h";
       display.style.display = "block";
       incorrectCity.style.display = "none";
+
+      // weather Icon
+      if(data.weather[0].main === "Clear"){
+        weatherIcon.src = "images/clear.png"
+      }
+      else if(data.weather[0].main === "Clouds"){
+        weatherIcon.src = "images/clouds.png"
+      }
+      else if(data.weather[0].main === "Drizzle"){
+        weatherIcon.src = "images/drizzle.png"
+      }
+      else if(data.weather[0].main === "Rain"){
+        weatherIcon.src = "images/rain.png"
+      }
+      else if(data.weather[0].main === "Snow"){
+        weatherIcon.src = "images/snow.png"
+      }
+      else{
+        weatherIcon.src = "images/mist.png"
+      }
+
+      
     } else {
       display.style.display = "none";
       incorrectCity.style.display = "block";
